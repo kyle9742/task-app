894f9de05ef82909727311a2ea3f68abcc9d8dc5
it <kyle9742@naver.com>
2024-06-24 12:21:14 +0900

    06/21 TodoList react

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import TaskColumn from "./components/TaskColumn";
+import TaskForm from "./components/TaskForm";
+import todoIcon from './assets/direct-hit.png';
+import doingIcon from './assets/glowing-star.png';
+import doneIcon from './assets/check-mark-button.png';
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const saveTasks = localStorage.getItem('tasks');
+  const [tasks, setTasks] = useState(JSON.parse(saveTasks) || []);
+
+  useEffect(() => {
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+	}, [tasks]);
+
+  const handleDelete = (taskIndex) => {
+		const newTasks = tasks.filter((task, index) => index !== taskIndex);
+		setTasks(newTasks);
+	};
+
+  return (
+    <div className="app">
+      <TaskForm setTasks={setTasks} />
+      <header className="app_header">Header Section</header>
+      <main className="app_main">
+        <TaskColumn title="할 일" icon={todoIcon} tasks={tasks} status='todo' handleDelete={handleDelete} />
+        <TaskColumn title="진행중" icon={doingIcon} tasks={tasks} status='doing' handleDelete={handleDelete} />
+        <TaskColumn title="완 료" icon={doneIcon} tasks={tasks} status='done' handleDelete={handleDelete} />
+      </main>
+    </div>
+  );
+}
